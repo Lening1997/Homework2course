60318cb313acf60e567263139fbd4c584a8b1f47
Lening1997 <lening.darya@mail.ru>
2023-07-12 02:00:29 +0200

    Создала файл hw-2.js и решила задачи

---
+++ scripts/hw-2.js
@@ -0,0 +1,74 @@
+//Задание 1
+/*let a = 10;
+alert( a );
+a = 20;
+alert ( a );*/
+
+//Задание 2
+/*let date = "Год выпуска первого Iphone";
+alert ( date );*/
+
+//Задание 3
+/*let name = "Имя создателя языка JavaScript";
+alert ( name );*/
+
+//Задание 4
+/*let a = 10;
+let b = 2; 
+//alert ( a + b );
+//alert ( a - b );
+//alert ( a * b );
+alert ( a / b );*/
+
+//Задание 5
+/*let a = 2;
+let result = a ** 5;
+alert ( result );*/
+
+//Задание 6
+/*let a = 9;
+let b = 2;
+let result = a % b ;
+alert ( result );*/
+
+//Задание 7
+/*let num = 1;
+num += 5;
+num -= 3;
+num *= 7;
+num /= 3;
+num ++;
+--num;
+alert ( num );*/
+
+//Задание 8
+/*let age = Number(prompt ("Сколько вам лет?"));
+alert ( age );*/
+
+//Задание 9.0
+/*const user = {
+    name: Dasha,
+    age: 25,
+    isAdmin: true
+}
+
+//Задание 9.1
+user["city of residence"] = "Калининград";
+
+//Задание 9.2
+user.age = 30;
+
+//Задание 9.3
+delete user{"city of residence"};
+
+//Задание 9.4
+let info = prompt ('Какую информацию хотите узнать о пользователе?');
+console.log(user[info]);
+alert ( user[info] );*/
+
+//Задание 10
+
+/*let userName = prompt ('Введите Ваше имя');
+alert (`Привет, ${userName} !`);*/
+
+
