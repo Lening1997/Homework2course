46117580c821ca2e4e38863336a26dafffb0507c
Lening1997 <lening.darya@mail.ru>
2023-07-15 00:54:03 +0200

    Создан файл hw-3.js

---
+++ scripts/hw-3.js
@@ -0,0 +1,34 @@
+//Задание 1 
+/*let password = "barcelona";
+let input = prompt('Введите пароль');
+if ( input === password) {
+    alert ('Пароль введен верно');
+} else {
+    alert ('Пароль введен неправильно');
+}*/
+
+//Задание 2
+/*let c = prompt('Введите число');
+if (c > 0 && c < 10) {
+    alert ('Верно');
+} else {
+    alert ('Неверно');
+}*/
+
+//Задание 3
+/*let d = prompt ('Введите число');
+let e = prompt ('Введите число');
+if (d > 100 || e > 100) {
+    alert ('Верно');
+} else {
+    alert ('Неверно');
+}*/
+
+//Задание 4
+/*let a = '2';
+let b = '3';
+a = Number(a);
+b = Number(b);
+alert (a + b);*/
+
+//Задание 5
