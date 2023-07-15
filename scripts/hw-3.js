af0fd7010527134edfb8fe9157080de5887c2ad5
Lening1997 <lening.darya@mail.ru>
2023-07-15 23:13:52 +0200

    Внесены изменения в hw-3.js

--- scripts/hw-3.js
+++ scripts/hw-3.js
@@ -32,3 +32,46 @@ b = Number(b);
 alert (a + b);*/
 
 //Задание 5
+let monthNumber = prompt('Введите номер месяца');
+switch (monthNumber) {
+    case '1' : 
+    console.log('зима');
+    break;
+    case '2' :
+    console.log('зима');
+    break;
+    case '3' : 
+    console.log('весна');
+    break;
+    case '4' : 
+    console.log('весна');
+    break;
+    case '5' : 
+    console.log('весна');
+    break;
+    case '6' : 
+    console.log('лето');
+    break;
+    case '7' : 
+    console.log('лето');
+    break;
+    case '8' : 
+    console.log('лето');
+    break;
+    case '9' : 
+    console.log('осень');
+    break;
+    case '10' : 
+    console.log('осень');
+    break;
+    case '11' : 
+    console.log('осень');
+    break;
+    case '12' : 
+    console.log('зима');
+    break;
+    default: console.log('ввел что-то не то');
+    break;
+}
+   
+
