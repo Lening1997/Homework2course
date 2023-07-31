a0b3cbb479c536d855c89220a3672f20ffd3abb3
Lening1997 <lening.darya@mail.ru>
2023-08-01 00:07:45 +0200

    Создан файл hw-7.js

---
+++ scripts/hw-7.js
@@ -0,0 +1,41 @@
+//Задание 1
+/*let str = 'Строка';
+str = str.toUpperCase();
+console.log(str);*/
+
+//Задание 2
+/*const names = ['Кошка Царапа', 'Собака Нора', 'Петух Кузя', 'Собака Ари', 'Кошка Бусинка', 'Петух Кукарику', 'Кошка Барселона'];
+const cat = 'Кошка';
+
+names.forEach((name) => {
+    if (name.toLowerCase().startsWith(cat.toLowerCase())) {
+		console.log(name);
+	}
+});*/
+
+//Задание 3
+/*const number = 32.58884;
+
+console.log(Math.floor(number));
+
+console.log(Math.ceil(number));
+
+console.log(Math.round(number));*/
+
+//Задание 4
+/*const numbers = [52, 53, 49, 77, 21, 32];
+
+Math.min(numbers);
+console.log(numbers);
+
+Math.max(numbers);
+console.log(numbers);*/
+
+//Задание 5
+/*function numbers(minValue, maxValue) {
+    return Math.round(Math.random() * maxValue);
+    }
+    
+    console.log(numbers(1, 10));*/
+
+//Задание 6
